52e936ca4cbb0efe166e0fed3f8574882b7fdb11
Deepak H K <deepakhk@aceanalytics.tech>
2021-03-04 17:46:02 +0530

    Improved the footer layout

--- src/footer.js
+++ src/footer.js
@@ -1,59 +1,66 @@
 import React from 'react';
 import './footer.css';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
 const footer = () => {
 
    return (
     <div class="footer-container">
-       <div class="footer-section1">
+       <div class="footer-section1 section-row">
            <h2>Ace Analytics</h2>
          <div class="section1-row1">
-            <a href="www.facebook.com">
+            <a class="a-color" href="www.facebook.com">
+              <FacebookIcon></FacebookIcon>
               <div> Facebook</div>
             </a>
          </div>
          <div class="section1-row2">
-           <a href="www.linkedIn.com">
+           <a class="a-color" href="www.linkedIn.com">
+             <LinkedInIcon></LinkedInIcon>
               <div> linkedIn</div>
             </a>
          </div>
          <div class="section1-row2">
-         <a href="www.youtube.com">
+         <a class="a-color" href="www.youtube.com">
+              <YouTubeIcon></YouTubeIcon>
               <div> You Tube</div>
             </a>
          </div>
        </div>
-       <div class="footer-section2">
+       <div class="footer-section2 section-row">
             <h3> Products</h3>
         <div class="section1-row1">
-        <a href="www.aceanlytics.com">
+        <a class="a-color" href="www.aceanlytics.com">
               <div> Ace clients</div>
             </a>
         </div>
         <div class="section1-row2">
-        <a href="www.aceanlytics.com">
+        <a class="a-color" href="www.aceanlytics.com">
               <div> Ace Products</div>
             </a>
         </div>
         <div class="section1-row2">
-        <a href="www.aceanlytics.com">
+        <a class="a-color" href="www.aceanlytics.com">
               <div> Ace Services</div>
             </a>
         </div>
        </div>
-       <div class="footer-section3">
+       <div class="footer-section3 section-row">
            <h3>Company</h3>
            <div class="section1-row1">
-           <a href="www.aceanlytics.com">
+           <a class="a-color" href="www.aceanlytics.com">
               <div> About us</div>
             </a>
             </div>
             <div class="section1-row2">
-            <a href="www.aceanlytics.com">
+            <a class="a-color" href="www.aceanlytics.com">
               <div> Careers</div>
             </a>
             </div>
             <div class="section1-row2">
-            <a href="www.aceanlytics.com">
+            <a class="a-color" href="www.aceanlytics.com">
               <div> Privacy</div>
             </a>
             </div>
